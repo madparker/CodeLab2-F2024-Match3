10d10b76e7ae76992ed88db3c9bafe08e857dd90
alexandravkanderson <ava8270@nyu.edu>
2024-09-06 12:35:14 -0400

    Move Tokens/Seed Generator Scripts Comments
    Team ALPHA commented the MoveTokenScript, DailySeedGenerator, SeedGenerator.

--- CodeLab2-Match3/Assets/Scripts/SeedGenerator/DailySeedGenerator.cs
+++ CodeLab2-Match3/Assets/Scripts/SeedGenerator/DailySeedGenerator.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class DailySeedGenerator : SeedGenerator {
 
-	void Awake(){
+	void Awake(){ //on awake, when you open it up, randomizing the seed based on the current date
 		int seed = System.DateTime.Now.Year * 1000 + System.DateTime.Now.DayOfYear;
 
-		Random.InitState(seed);
+		Random.InitState(seed); //picking a random seed based on the current date
 	}
 }
--- CodeLab2-Match3/Assets/Scripts/SeedGenerator/SeedGenerator.cs
+++ CodeLab2-Match3/Assets/Scripts/SeedGenerator/SeedGenerator.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SeedGenerator : MonoBehaviour {
 
-	public int initSeed;
+	public int initSeed; //the initial Seed as integer
+	//seed generator gets random numbers based on the start seed you choose
 
 	void Awake(){
-		Random.InitState(initSeed);
+		Random.InitState(initSeed); //randomizing the initial seed on awake
 	}
 }
