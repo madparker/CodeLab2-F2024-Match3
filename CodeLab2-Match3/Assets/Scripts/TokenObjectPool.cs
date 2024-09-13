5704bf30e44a98e866e69cdd2f43ceb98ea415ef
Matthew Parker <maddparker@gmail.com>
2024-09-13 13:49:57 -0400

    Updated ObjectPool to actuially pool objects

--- CodeLab2-Match3/Assets/Scripts/TokenObjectPool.cs
+++ CodeLab2-Match3/Assets/Scripts/TokenObjectPool.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class TokenObjectPool : MonoBehaviour
 {
-    protected Object[] tokenTypes;
+    Queue<GameObject> objectPool = new Queue<GameObject>();
+    
+    Object[] tokenTypes;
+    Sprite[] spriteTypes; 
     
     // Start is called before the first frame update
     void Start()
     {
         tokenTypes = (Object[])Resources.LoadAll("_Core/Tokens/"); //load all the token prefabs
+        spriteTypes = Resources.LoadAll<Sprite>("_Core/Images");
     }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -20,16 +24,37 @@ public class TokenObjectPool : MonoBehaviour
 
     public GameObject GetToken(Vector3 position)
     {
-        GameObject token = 
-            Instantiate(tokenTypes[Random.Range(0, tokenTypes.Length)], 
-                position, 
-                Quaternion.identity) as GameObject;
+        GameObject token;
+        
+        if (objectPool.Count == 0)
+        {
+            token =
+                Instantiate(tokenTypes[Random.Range(0, tokenTypes.Length)],
+                    position,
+                    Quaternion.identity) as GameObject;
+        }
+        else
+        {
+            token = objectPool.Dequeue();
+            Reset(token, position);
+        }
 
         return token;
     }
 
     public void RemoveToken(GameObject token)
     {
-        Destroy(token);
+        token.SetActive(false);
+        
+        objectPool.Enqueue(token);
+    }
+
+    public void Reset(GameObject token, Vector3 position)
+    {
+        token.SetActive(true);
+        token.transform.position = position;
+        Sprite newSprite = spriteTypes[Random.Range(0, spriteTypes.Length)];
+        token.GetComponent<SpriteRenderer>().sprite = newSprite;
+        token.name = newSprite.name;
     }
 }
