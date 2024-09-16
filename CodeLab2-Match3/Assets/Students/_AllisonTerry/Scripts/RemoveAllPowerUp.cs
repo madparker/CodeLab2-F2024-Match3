37321e223d4c45b480b91b6167a521389eb17cf1
AlleyTerry <art8689@nyu.edu>
2024-09-16 13:56:13 -0400

    power up acquired B)

--- CodeLab2-Match3/Assets/Students/_AllisonTerry/Scripts/RemoveAllPowerUp.cs
+++ CodeLab2-Match3/Assets/Students/_AllisonTerry/Scripts/RemoveAllPowerUp.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using AllisonTerry;
 using UnityEngine;
 
-public class RemoveAllPowerUp : InputManagerScript
+public class RemoveAllPowerUp : MonoBehaviour
 {
     protected FixedMatchManagerScript myMatchManagerScript;
     protected GameManagerScript gameManager;
-    protected InputManagerScript inputManager;
+    protected TokenObjectPool tokenObjectPool;
     public GameObject removeAllPowerUpButton;
-    public GameObject currentToken;
+    GameObject[] tokenTypes;
+    Sprite[] spriteTypes;
+    public int powerUpThreshold = 10;
     
     
     // Start is called before the first frame update
@@ -18,12 +20,14 @@ public class RemoveAllPowerUp : InputManagerScript
     {
         myMatchManagerScript = GetComponent<FixedMatchManagerScript>();
         gameManager = GetComponent<GameManagerScript>();
+        tokenTypes = Resources.LoadAll<GameObject>("_Core/Tokens"); //load all the token prefabs
+        spriteTypes = Resources.LoadAll<Sprite>("_Core/Images");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (myMatchManagerScript.matchNumber >= 5)
+        if (myMatchManagerScript.matchNumber >= powerUpThreshold)
         {
             //remove all button active
             removeAllPowerUpButton.gameObject.SetActive(true);
@@ -34,33 +38,35 @@ public class RemoveAllPowerUp : InputManagerScript
     public void RemoveAllTokensOfType()
     {
         
+        //have two game objects one from the list of tokens
+        GameObject tokenToBeChecked;
+        //chooses a random token from the list
+        tokenToBeChecked = tokenTypes[Random.Range(0, spriteTypes.Length)];
+        //the other to be initalized later in the loop to then be destroyed
+        GameObject tokenToBeRemoved;
+        print(tokenToBeChecked);
+        
         //take the sprite render of the sprite chosen
         // go through the entire array
         for (int x = 0; x < gameManager.gridWidth; x++)
         {
             for (int y = 0; y < gameManager.gridHeight; y++)
             {
-                // add all the tokens of that type to the to be removed list
-                
+                //set a place holder sprite equal to the current sprite the loop is at
+                Sprite currentSprite = gameManager.gridArray[x, y].GetComponent<SpriteRenderer>().sprite;
+                //if the current sprite is equal to the sprite from the list of tokens
+                if (currentSprite == tokenToBeChecked.GetComponent<SpriteRenderer>().sprite)
+                {
+                    //set the token to be removed to the current token in the array
+                    tokenToBeRemoved = gameManager.gridArray[x, y];
+                    //tokenObjectPool.RemoveToken(tokenToBeRemoved); didn't work :(
+                    //remove that token
+                    Destroy(tokenToBeRemoved);
+                }
             }
         }
-        
-        //remove them
-    }
-
-    public void getCurrentToken()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //sets mousePos variable to the current location of the mouse on screen
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            //assigns the tokenCollider variable to the specific token that you are mousing over
-            Collider2D tokenCollider = Physics2D.OverlapPoint(mousePos);
-
-            currentToken = tokenCollider.gameObject;
-            print(currentToken.name);
-        }
-
+        myMatchManagerScript.matchNumber = 0;
+        removeAllPowerUpButton.SetActive(false);
+        powerUpThreshold *= 2;
     }
 }
